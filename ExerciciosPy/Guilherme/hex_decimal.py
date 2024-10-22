9271644335f550271f2709303f20b9d29c535751
Gabriel P <peixotopridemode@gmail.com>
2024-10-21 21:32:26 -0300

    add hexadecimal

--- ExerciciosPy/Guilherme/hex_decimal.py
+++ ExerciciosPy/Guilherme/hex_decimal.py
@@ -1,1 +1,11 @@
 
+tipo = int(input("Digite 1 para numero de registro 2 para hexadecimal"))
+numero = input("Qual o numero quer converter ")
+
+if tipo == 1:
+    numRes = int(numero,16)
+    print(f"Seu numero de registro é: {numRes}")
+elif tipo == 2:
+    numHex = hex(int(numero))
+    print(f"Seu numero em hexadecimal é: {numHex}")
+    
